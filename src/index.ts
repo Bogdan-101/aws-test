d56a11f861af5832133be6486efd9f561527e695
Bogdan Karmyzow <big_bogdan@rambler.ru>
2021-11-22 11:52:51 +0300

    feat: configured project dependencies

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(
+    "/",
+    async (_req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Server is working!",
+        });
+    }
+);
+
+try {
+    app.listen(port, (): void => {
+        console.log(`Connected successfully on port ${port}`);
+    });
+} catch ({message}) {
+    console.error(`Error occurred: ${message}`);
+}
+
+
+
+
+
+
+
