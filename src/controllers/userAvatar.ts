cc14c827e24fb9cb327960b1592527cbfb1e2bf5
Bogdan Karmyzow <big_bogdan@rambler.ru>
2021-11-23 18:24:22 +0300

    feat: trying to fix seq error

--- src/controllers/userAvatar.ts
+++ src/controllers/userAvatar.ts
@@ -4,25 +4,25 @@ import {
   UserAvatarAttributes,
   UserAvatarCreationAttributes,
 } from "../models/userAvatar";
-import {BUCKET_NAME, s3} from "../s3bucket";
-import {AWSError} from "aws-sdk";
-import {GetObjectOutput} from "aws-sdk/clients/s3";
+import { BUCKET_NAME, s3 } from "../s3bucket";
+// import { AWSError } from "aws-sdk";
+// import { GetObjectOutput } from "aws-sdk/clients/s3";
 
 const multer = require("multer");
-const multerS3 = require('multer-s3')
+const multerS3 = require("multer-s3");
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: BUCKET_NAME,
     metadata: function (_req: any, file: any, cb: Function) {
-      cb(null, {fieldName: file.fieldname});
+      cb(null, { fieldName: file.fieldname });
     },
     key: function (_req: any, file: any, cb: Function) {
-      cb(null, `${Date.now().toString()}-${file.originalname}`)
-    }
-  })
-})
+      cb(null, `${Date.now().toString()}-${file.originalname}`);
+    },
+  }),
+});
 
 const categoryApi = (app: express.Application, db: any) => {
   app.get("/userAvatar", (_req: express.Request, res: express.Response) =>
@@ -32,23 +32,37 @@ const categoryApi = (app: express.Application, db: any) => {
     )
   );
 
-  app.get("/userAvatar/:userId", async (req: express.Request, res: express.Response) => {
+  app.get(
+    "/userAvatar/:userId",
+    (req: express.Request, res: express.Response) => {
       const userId: number = +req.params.userId;
 
-      const user: UserAvatarAttributes = await db.UserAvatar.findOne({where: {userId: userId}});
-      if (user === null) {
-        res.send('User not found');
-      }
+      db.UserAvatar.findOne({ where: { userId: userId } }).then(
+        (
+          user: Model<
+            UserAvatarAttributes,
+            UserAvatarCreationAttributes
+          > | null
+        ) => {
+          if (user === null) {
+            res.send("User not found");
+          }
 
-      s3.getObject({Bucket: BUCKET_NAME, Key: user.avatarPath}, function(err: AWSError, data:GetObjectOutput) {
-        if (err) {
-          res.send('An error occurred');
+          res.send(user)
+
+          // s3.getObject(
+          //   { Bucket: BUCKET_NAME, Key: user?.get('avatarPath') },
+          //   function (err: AWSError, data: GetObjectOutput) {
+          //     if (err) {
+          //       res.send("An error occurred");
+          //     } else {
+          //       res.send(data);
+          //     }
+          //   }
+          // );
         }
-        else {
-          res.send(data)
-        }
-      });
-  }
+      );
+    }
   );
 
   app.delete("/userAvatar", (req: express.Request, res: express.Response) => {
